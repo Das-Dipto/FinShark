cc2a8c1485c8f23f1111acd54600a26abad8c849
D8S <ddipto396@gmail.com>
2026-02-19 23:59:11 +0600

    Add ef core and integrated with SSMS

--- api/Program.cs
+++ api/Program.cs
@@ -1,3 +1,8 @@
+using api.Data;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDBContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
