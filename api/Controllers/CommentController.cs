cfb6ed72733c7dcf656afe521518c20bba4f8e3c
D8S <ddipto396@gmail.com>
2026-04-15 23:59:32 +0600

    DELETE method implemented for comment

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -74,9 +74,20 @@ namespace api.Controllers
 
             return Ok(comment.ToCommentDto());
         }
-    }
 
-    // Delete method work started
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var commentModel = await _commentRepo.DeleteAsync(id);
 
-    
+            if (commentModel == null)
+            {
+                return NotFound("Comment does not exist");
+            }
+
+            return Ok(commentModel);
+        }
+    }   
+
 }
