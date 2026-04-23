9b1bb9222565e716b685b3d73444bfd8a45c3a0b
D8S <ddipto396@gmail.com>
2026-04-23 23:42:34 +0600

    Register work started

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -20,17 +20,24 @@ namespace api.Data
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            // ✅ VERY IMPORTANT: keep Identity configuration
-            base.OnModelCreating(modelBuilder);
+            base.OnModelCreating(builder);
 
-            // Comment -> Stock (Many-to-One)
-            modelBuilder.Entity<Comment>()
-                .HasOne(c => c.Stock)
-                .WithMany(s => s.Comments)
-                .HasForeignKey(c => c.StockId)
-                .OnDelete(DeleteBehavior.Cascade);
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                },
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
