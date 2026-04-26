cec92ef0fdc063ae2bc74a63d0212ac100f08eb1
D8S <ddipto396@gmail.com>
2026-04-26 23:52:00 +0600

    Portfolio many to many relation work started

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace api.Models
 {
-    // ISSUE might exist into this file
+    
+    [Table("Comments")]
     public class Comment
     {
         public int Id {get; set;}
