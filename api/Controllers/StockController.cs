5a587ddfae870f22c27089ed8d990d5ea458449b
D8S <ddipto396@gmail.com>
2026-04-27 00:12:07 +0600

    PortfolioManytoMany relations implemented

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -26,7 +26,7 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        [Authorize]
+        // [Authorize]
         public async Task<IActionResult> GetAll([FromQuery] QueryObject query)
         {
             if (!ModelState.IsValid)
