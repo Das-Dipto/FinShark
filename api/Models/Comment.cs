9c01a8e56e0b794c3411db06f7a1ffb9628b192d
D8S <ddipto396@gmail.com>
2026-04-29 23:57:26 +0600

    One to One relation made based on AppUser

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -17,6 +17,8 @@ namespace api.Models
         public DateTime CreateOn {get; set;} = DateTime.Now;
         public int? StockId {get; set;}
         public Stock? Stock {get; set;}
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
 
     }
 }
