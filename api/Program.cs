822da034bcdb906e634563579ef053b964846e62
D8S <ddipto396@gmail.com>
2026-04-30 23:06:00 +0600

    Data Seed work done

--- api/Program.cs
+++ api/Program.cs
@@ -65,6 +65,9 @@ builder.Services.AddAuthentication(options =>
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 builder.Services.AddScoped<IPortfolioRepository, PortfolioRepository>();
+// builder.Services.AddScoped<IFMPService, FMPService>();
+// builder.Services.AddHttpClient<IFMPService, FMPService>();
+
 
 var app = builder.Build();
 
